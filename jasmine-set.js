b0792f3ba87977412b4043028e68b9241a8e5a02
Joe Vennix <joe_vennix@rapid7.com>
2014-05-05 12:48:04 -0500

    Rebuild and bump version.

--- jasmine-set.js
+++ jasmine-set.js
@@ -1,5 +1,5 @@
 //
-// jasmine-beforeSuite - 0.1.0
+// jasmine-set - 0.1.0
 //
 // A plugin for the Jasmine behavior-driven Javascript testing framework that
 // adds a `set` global function. It is inspired by rspec's very nice `let` syntax.
@@ -10,7 +10,7 @@
 //
 // @author Joe Vennix
 // @copyright Rapid7 2014
-// @see https://github.com/jvennix-r7/jasmine-beforeSuite
+// @see https://github.com/jvennix-r7/jasmine-set
 //
 // Released under the MIT License.
 //
@@ -18,14 +18,35 @@
 (function() {
   var context, install, jasmine;
 
-  install = function(_, jasmine) {
-    var context, finish, globalPatches, namespaceStack, suites;
+  install = function(_, jasmine, beforeSuite, afterSuite) {
+    var context, globalPatches, namespaceStack, suites;
     suites = {};
     namespaceStack = {};
     context = this;
     globalPatches = {
       set: function(name, opts, fn) {
         var ret;
+        beforeSuite(function() {
+          var id, obj, _ref, _ref1, _ref2;
+          id = jasmine != null ? (_ref = jasmine.getEnv()) != null ? (_ref1 = _ref.currentSpec) != null ? (_ref2 = _ref1.suite) != null ? _ref2.id : void 0 : void 0 : void 0 : void 0;
+          obj = _.find(suites[id], function(obj) {
+            return obj.name === name;
+          });
+          if (obj == null) {
+            return;
+          }
+          namespaceStack[name] || (namespaceStack[name] = []);
+          namespaceStack[name].push(obj);
+          return obj != null ? typeof obj.fn === "function" ? obj.fn() : void 0 : void 0;
+        });
+        afterSuite(function() {
+          var _ref, _ref1;
+          delete context[name];
+          if ((_ref = namespaceStack[name]) != null) {
+            _ref.pop();
+          }
+          return (_ref1 = _.last(namespaceStack[name])) != null ? typeof _ref1.fn === "function" ? _ref1.fn() : void 0 : void 0;
+        });
         if (_.isFunction(opts)) {
           fn = opts;
           opts = null;
@@ -35,9 +56,16 @@
           opts.now = false;
         }
         ret = function(fn) {
-          var doit, id, _ref, _ref1;
+          var doit, id, oncePerSuiteWrapper, setter, _ref, _ref1;
+          setter = function(x) {
+            console.log("CALLING SETTER!");
+            oncePerSuiteWrapper();
+            delete context[name];
+            return context[name] = x;
+          };
+          oncePerSuiteWrapper = null;
           doit = function() {
-            var cachedId, cachedResult, oncePerSuiteWrapper;
+            var cachedId, cachedResult;
             if (opts.now) {
               return context[name] = fn();
             } else {
@@ -54,6 +82,7 @@
               };
               return Object.defineProperty(context, name, {
                 get: oncePerSuiteWrapper,
+                set: setter,
                 configurable: true
               });
             }
@@ -72,39 +101,21 @@
         }
       }
     };
-    _.extend(this, globalPatches);
-    beforeEach(function() {
-      var id, _ref, _ref1, _ref2;
-      id = jasmine != null ? (_ref = jasmine.getEnv()) != null ? (_ref1 = _ref.currentSpec) != null ? (_ref2 = _ref1.suite) != null ? _ref2.id : void 0 : void 0 : void 0 : void 0;
-      return _.each(suites[id], function(obj) {
-        var _name;
-        namespaceStack[_name = obj.name] || (namespaceStack[_name] = []);
-        namespaceStack[obj.name].push(obj);
-        return obj.fn();
-      });
-    });
-    finish = jasmine.Suite.prototype.finish;
-    return jasmine.Suite.prototype.finish = function(cb) {
-      _.each(suites[this.id], function(obj) {
-        var _ref, _ref1;
-        delete context[obj.name];
-        if ((_ref = namespaceStack[obj.name]) != null) {
-          _ref.pop();
-        }
-        return (_ref1 = _.last(namespaceStack[obj.name])) != null ? typeof _ref1.fn === "function" ? _ref1.fn() : void 0 : void 0;
-      });
-      return finish.call(this, cb);
-    };
+    return _.extend(this, globalPatches);
   };
 
   context = (typeof window === "object" && window) || (typeof global === "object" && global) || this;
 
   jasmine = context.jasmine || require("jasmine");
 
+  if (this.beforeSuite == null) {
+    require("jasmine-before-suite");
+  }
+
   if (jasmine == null) {
-    console.error("jasmine-beforeSuite: Jasmine must be required first. Aborting.");
+    console.error("jasmine-set: Jasmine must be required first. Aborting.");
   } else {
-    install.call(context, context._ || require("underscore"), jasmine);
+    install.call(context, context._ || require("underscore"), jasmine, context.beforeSuite, context.afterSuite);
   }
 
 }).call(this);
